8ab6336be95a541de171b53f431d015d1c3ffdca
Ryan Mendoza <ryanmendoza925@gmail.com>
2017-11-15 20:07:41 -0500

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Hangman-Game
+# Hangman Game
+
+[Link to Game](https://rjm925.github.io/Hangman-Game)
+
+This is a hangman game of the current 30 NBA teams. The user has up to 6 incorrect letter guesses until he or she losses. The game keeps track of the user's wins and losses for the session.
+
+# Technology Used
+* HTML
+* CSS
+* Javascript
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,22 @@
-var wins = 0;       //Win counter
-var losses = 0;     //Loss counter
-var remaining;      //Guesses remaining
-var guessed;        //Array of letters guessed
-var revealed;       //Array of letters revealed
-var answer;         //Random word for game
-var lastWord;       //Display word of last round
+let wins = 0;       // Win counter
+let losses = 0;     // Loss counter
+let remaining;      // Guesses remaining
+let guessed;        // Array of letters guessed
+let revealed;       // Array of letters revealed
+let answer;         // Random team for round
+let lastWord;       // Display team of last round
 
-//Array of letters
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//Array of teams
-var teams = ["hawks", "celtics", "nets", "hornets", "bulls", "cavaliers", "mavericks", "nuggets", "pistons", "warriors", "rockets", "pacers", "clippers", "lakers", "grizzlies", "heat", "bucks", "timberwolves", "pelicans", "knicks", "thunder", "magic", "sixers", "suns", "trailblazers", "kings", "spurs", "raptors", "jazz", "wizards"];
+// Array of letters
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Array of teams
+const teams = ["hawks", "celtics", "nets", "hornets", "bulls", "cavaliers", "mavericks", "nuggets", "pistons", "warriors", "rockets", "pacers", "clippers", "lakers", "grizzlies", "heat", "bucks", "timberwolves", "pelicans", "knicks", "thunder", "magic", "sixers", "suns", "trailblazers", "kings", "spurs", "raptors", "jazz", "wizards"];
 
-//Sound played when player wins
-var winSound = new sound("assets/media/winner.mp3");
-//Sound played when player losses
-var loseSound = new sound("assets/media/loser.mp3");
+// Sound played when player wins
+const winSound = new sound("assets/media/winner.mp3");
+// Sound played when player losses
+const loseSound = new sound("assets/media/loser.mp3");
 
-//Load sound into variable
+// Load sound into variable
 function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
@@ -32,139 +32,141 @@ function sound(src) {
     }    
 }
 
-//Start of new round
+// Start of new round
 function newGame() {
-  //Player can guess 10 wrong letters
-  remaining = 10;
-  //Displays guesses remaining
+  // Player can guess 6 wrong letters
+  remaining = 6;
+  // Displays guesses remaining
   remain.innerHTML = "Guesses Left " + remaining;
-  //Random team for player to guess
+  // Random team for player to guess
   answer = teams[Math.floor(Math.random() * teams.length)];
-  //Array that fills up with letters guessed
+  // Array that fills up with letters guessed
   guessed = [];
-  //Array of characters player needs to guess
+  // Array of characters player needs to guess
   revealed = [];
-  //Loops through length of string
-  for (var i = 0; i < answer.length; i++) {
-    //Initializes the word by displaying blanks for each letter
+  // Loops through length of string
+  for (let i = 0; i < answer.length; i++) {
+    // Initializes the word by displaying blanks for each letter
     revealed.push("_");
   }
-  //Displays wins
+  // Displays wins
   W.innerHTML = wins;
-  //Displays losses
+  // Displays losses
   L.innerHTML = losses;
 }
 
-//Waits for key press
+// Waits for key press
 document.onkeyup = function(event) {
-  //Stores key press in variable and makes lowerCase
-  var guess = event.key.toLowerCase();
+  // Stores key press in variable and makes lowerCase
+  let guess = event.key.toLowerCase();
 
-  //Variable to verify key pressed is a letter and has not been guessed
-  var check = false;
-  //Loops through array of letters
-  for (var i = 0; i < alphabet.length; i++) {
-    //Compares key press to every letter in array
+  // Variable to verify key pressed is a letter and has not been guessed
+  let check = false;
+  // Loops through array of letters
+  for (let i = 0; i < alphabet.length; i++) {
+    // Compares key press to every letter in array
     if (guess === alphabet[i]) {
-      //If letter is found, verifies key press is a letter
+      // If letter is found, verifies key press is a letter
       check = true;
     }
   }
 
-  //Loop to see if letter was already guessed
-  for (var i = 0; i < guessed.length; i++) {
-    //Checks array of guessed with key pressed
+  // Loop to see if letter was already guessed
+  for (let i = 0; i < guessed.length; i++) {
+    // Checks array of guessed with key pressed
     if (" " + guess.toUpperCase() === guessed[i]) {
-      //If already guessed check returns to false even if it is a letter
+      // If already guessed check returns to false even if it is a letter
       check = false;
     }
   }
 
-  //Variable to see if letter is in hidden word
-  var found = false;
-  //If key press is a letter and not already guessed
+  // Variable to see if letter is in hidden word
+  let found = false;
+  // If key press is a letter and not already guessed
   if (check) {
-    //Loops through hidden word
-    for (var i = 0; i < answer.length; i++) {
-      //If letter found
+    // Loops through hidden word
+    for (let i = 0; i < answer.length; i++) {
+      // If letter found
       if (guess === answer.charAt(i)) {
-        //Changes _ to key press in revealed array
+        // Changes _ to key press in revealed array
         revealed[i] = guess.toUpperCase();
-        //Letter is found
+        // Letter is found
         found = true;
       }
     }
 
-    //If letter is found
+    // Variable to check if word is guessed
+    let blank;
+    // If letter is found
     if (found) {
-      //Loops to see if all letters are guessed
-      for (var i = 0; i < revealed.length; i++) {
-        //If there is still a letter missing
+      // Loops to see if all letters are guessed
+      for (let i = 0; i < revealed.length; i++) {
+        // If there is still a letter missing
         if (revealed[i] === "_") {
-          //Returns that word is not completely guessed
-          var blank = true;
-          //Breaks out of loop
+          // Returns that word is not completely guessed
+          blank = true;
+          // Breaks out of loop
           break;
         }
-        //No blanks left
+        // No blanks left
         else {
-          //Returns that word is figured out
-          var blank = false;
+          // Returns that word is figured out
+          blank = false;
         }
       }
-      //If player guessed word
+      // If player guessed word
       if (blank === false) {
-        //Increase win counter
+        // Increase win counter
         wins++;
-        //Display new wins total
+        // Display new wins total
         W.innerHTML = wins;
-        //Play win sound
+        // Play win sound
         winSound.play();
-        //Stores word in variable 
+        // Stores word in variable 
         lastWord = answer.toUpperCase();
-        //Displays last word under wins
+        // Displays last word under wins
         lastWin.innerHTML = "Last Answer<br><br>" + lastWord;
-        //Removes word under losses
+        // Removes word under losses
         lastLost.innerHTML = "";
-        //Start a new game
+        // Start a new game
         newGame();
       }
     }
 
-    //If letter is incorrect
+    // If letter is incorrect
     if (found === false) {
-      //Decreases guesses remaining
+      // Decreases guesses remaining
       remaining--;
-      //Displays guesses remaining
+      // Displays guesses remaining
       remain.innerHTML = "Guesses Left " + remaining;
-      //Adds the letter to guessed array
+      // Adds the letter to guessed array
       guessed.push(" " + guess.toUpperCase());
     }
 
-    //Check if player still has guesses available
+    // Check if player still has guesses available
     if (remaining === 0) {
-      //Increment losses counter
+      // Increment losses counter
       losses++;
-      //Display new losses total
+      // Display new losses total
       L.innerHTML = losses;
-      //Play loss sound
+      // Play loss sound
       loseSound.play();
-      //Stores word in variable
+      // Stores word in variable
       lastWord = answer.toUpperCase();
-      //Removes displayed word under wins
+      // Removes displayed word under wins
       lastWin.innerHTML = "";
-      //Display last word under losses
+      // Display last word under losses
       lastLost.innerHTML = "Last Answer<br><br>" + lastWord;
-      //Start a new game
+      // Start a new game
       newGame();
     }
 
-    //Updates HTML to display hidden words/revealed letters
+    // Updates HTML to display hidden words/revealed letters
     reveal.innerHTML = revealed.join("  ");
-    //Updates HTML to display incorrect letters
+    // Updates HTML to display incorrect letters
     notFound.innerHTML = guessed.join("  ");
   }
 }
 
-//Start first game
+// Start first game
 newGame();
